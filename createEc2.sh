5df69b05a9b02d7af2c75490dccf1ec104e675c7
chethan2010 <hemanthkumarsaketi@gmail.com>
2024-11-05 16:13:01 +0530

    Roboshop-shell

---
+++ createEc2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#she bang will execute sript in  Linux
+
+instance=("mongodb" "catalog" "mysql" "rabbitMq" "catalogue" "user" "cart" "shipping" "payment" "web")
+
+
+for name in ${instance[@]}; Devops
+    echo "creating instance for: $name"
+
+done
